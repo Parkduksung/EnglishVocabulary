b13083d1d19807e573bdf9e4713e41f655d33ff6
Parkduksung <duksung1234@naver.com>
2021-05-19 23:26:05 +0900

    [feature-quiz] quiz 세팅.

--- app/src/main/java/com/example/englishvocabulary/ui/home/quiz/QuizFragment.kt
+++ app/src/main/java/com/example/englishvocabulary/ui/home/quiz/QuizFragment.kt
@@ -2,14 +2,22 @@ package com.example.englishvocabulary.ui.home.quiz
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.example.englishvocabulary.R
 import com.example.englishvocabulary.base.BaseFragment
 import com.example.englishvocabulary.databinding.FragmentQuizBinding
+import com.example.englishvocabulary.viewmodel.QuizViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class QuizFragment : BaseFragment<FragmentQuizBinding>(R.layout.fragment_quiz) {
+
+    private val quizViewModel by viewModels<QuizViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
     }
 
     companion object {
---
+++ app/src/main/java/com/example/englishvocabulary/viewmodel/QuizViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.englishvocabulary.viewmodel
+
+import android.app.Application
+import com.example.englishvocabulary.base.BaseViewModel
+import com.example.englishvocabulary.data.repository.ExcelVocaRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class QuizViewModel @Inject constructor(
+    app: Application,
+    private val excelVocaRepository: ExcelVocaRepository
+) : BaseViewModel(app) {
+
+}
