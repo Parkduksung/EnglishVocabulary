d8f410201ecaad8a2e689121b4a74cbbb5b3ecc1
Parkduksung <duksung1234@naver.com>
2021-07-15 17:55:10 +0900

    [refactor] 기본 안드로이드 설정이 다크모드일 경우 적용 안되게 수정.

--- app/src/main/java/com/example/englishvocabulary/base/BaseActivity.kt
+++ app/src/main/java/com/example/englishvocabulary/base/BaseActivity.kt
@@ -1,10 +1,11 @@
 package com.example.englishvocabulary.base
 
+import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
@@ -13,6 +14,7 @@ abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId
 
     protected lateinit var binding: B
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,6 +23,9 @@ abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId
         binding = DataBindingUtil.setContentView(this, layoutId)
         binding.lifecycleOwner = this
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         setContentView(binding.root)
     }
+
 }
