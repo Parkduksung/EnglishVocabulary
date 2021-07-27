47dd79e8dc0a27bfe644c62234af921eb1ab6e18
Parkduksung <duksung1234@naver.com>
2021-07-28 02:37:33 +0900

    [test] 불필요한 부분 제거.

--- app/src/test/java/com/example/englishvocabulary/ui/splash/SplashViewModelTest.kt
+++ app/src/test/java/com/example/englishvocabulary/ui/splash/SplashViewModelTest.kt
@@ -1,31 +1,4 @@
 package com.example.englishvocabulary.ui.splash
 
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.HiltTestApplication
-import kotlinx.coroutines.runBlocking
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
-@HiltAndroidTest
-@Config(application = HiltTestApplication::class)
-@RunWith(RobolectricTestRunner::class)
-class SplashViewModelTest {
-
-    @get:Rule
-    var hiltRule = HiltAndroidRule(this)
-
-    @Before
-    fun init() : Unit = runBlocking{
-        hiltRule.inject()
-    }
-
-    @Test
-    fun start(){
-
-    }
-}
+class SplashViewModelTest
