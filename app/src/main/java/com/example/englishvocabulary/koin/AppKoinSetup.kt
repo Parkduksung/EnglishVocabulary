6582a0685c0c818e9ca926dacdfa1e622bb7fd20
Parkduksung <duksung1234@naver.com>
2021-07-28 02:02:17 +0900

    [refactor] ApiModule 추가.

--- app/src/main/java/com/example/englishvocabulary/koin/AppKoinSetup.kt
+++ app/src/main/java/com/example/englishvocabulary/koin/AppKoinSetup.kt
@@ -1,8 +1,12 @@
 package com.example.englishvocabulary.koin
 
+import com.example.englishvocabulary.network.api.KakaoApi
+import com.example.englishvocabulary.network.api.NaverApi
 import com.example.englishvocabulary.ui.splash.SplashInteractor
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class AppKoinSetup : KoinBaseSetup() {
 
@@ -11,10 +15,34 @@ class AppKoinSetup : KoinBaseSetup() {
     }
 
 
+    private val apiModule = module {
+        single<KakaoApi> {
+            Retrofit.Builder()
+                .baseUrl(KAKAO_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(KakaoApi::class.java)
+        }
+        single<NaverApi> {
+            Retrofit.Builder()
+                .baseUrl(NAVER_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(NaverApi::class.java)
+        }
+    }
+
+
     override fun getModules(): List<Module> {
         return listOf(
-            interactorModule
+            interactorModule,
+            apiModule
         )
     }
 
+    companion object{
+        private const val KAKAO_URL = "https://dapi.kakao.com/"
+        private const val NAVER_URL = "https://openapi.naver.com/"
+
+    }
 }
