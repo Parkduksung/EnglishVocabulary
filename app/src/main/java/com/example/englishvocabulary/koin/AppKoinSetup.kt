14c5cdd08657335eed08408a7bca2a3fb6f48b2b
Parkduksung <duksung1234@naver.com>
2021-07-28 02:05:18 +0900

    [refactor] ViewModelModule 추가.

--- app/src/main/java/com/example/englishvocabulary/koin/AppKoinSetup.kt
+++ app/src/main/java/com/example/englishvocabulary/koin/AppKoinSetup.kt
@@ -5,7 +5,13 @@ import com.example.englishvocabulary.network.api.KakaoApi
 import com.example.englishvocabulary.network.api.NaverApi
 import com.example.englishvocabulary.network.room.database.BookmarkDatabase
 import com.example.englishvocabulary.network.room.database.ExcelVocaDatabase
+import com.example.englishvocabulary.ui.home.bookmark.BookmarkViewModel
+import com.example.englishvocabulary.ui.home.quiz.QuizViewModel
+import com.example.englishvocabulary.ui.home.search.SearchViewModel
+import com.example.englishvocabulary.ui.home.study.StudyViewModel
 import com.example.englishvocabulary.ui.splash.SplashInteractor
+import com.example.englishvocabulary.ui.splash.SplashViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -15,6 +21,14 @@ class AppKoinSetup : KoinBaseSetup() {
 
     private val interactorModule = module {
         factory { SplashInteractor() }
+    }
+
+    private val viewModelModule = module {
+        viewModel { SplashViewModel(get()) }
+        viewModel { StudyViewModel(get()) }
+        viewModel { SearchViewModel(get()) }
+        viewModel { QuizViewModel(get()) }
+        viewModel { BookmarkViewModel(get()) }
     }
 
 
