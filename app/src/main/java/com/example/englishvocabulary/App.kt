0d85edb784b9e8164cdf0074f3a4a054a9937d37
Parkduksung <duksung1234@naver.com>
2021-07-28 02:22:41 +0900

    [refactor] App 수정.

--- app/src/main/java/com/example/englishvocabulary/App.kt
+++ app/src/main/java/com/example/englishvocabulary/App.kt
@@ -2,13 +2,14 @@ package com.example.englishvocabulary
 
 import android.app.Application
 import android.content.Context
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
 class App : Application() {
+
+    private val appBaseApplication = AppBaseApplication()
 
     override fun onCreate() {
         super.onCreate()
+        appBaseApplication.onCreate(this)
         instance = this
     }
 
