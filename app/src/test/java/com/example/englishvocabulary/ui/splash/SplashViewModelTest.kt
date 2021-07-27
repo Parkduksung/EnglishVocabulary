7d69e68b10b13e5551a28a145ad8823043efe872
Parkduksung <duksung1234@naver.com>
2021-07-28 02:56:17 +0900

    [test] SplashViewModelTest 세팅.

--- app/src/test/java/com/example/englishvocabulary/ui/splash/SplashViewModelTest.kt
+++ app/src/test/java/com/example/englishvocabulary/ui/splash/SplashViewModelTest.kt
@@ -1,4 +1,37 @@
 package com.example.englishvocabulary.ui.splash
 
+import base.ViewModelBaseTest
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
-class SplashViewModelTest
+
+@RunWith(MockitoJUnitRunner::class)
+class SplashViewModelTest : ViewModelBaseTest() {
+
+    @Mock
+    lateinit var splashInteractor: SplashInteractor
+
+    private lateinit var splashViewModel: SplashViewModel
+
+    override fun createModules(): List<Module> {
+        return listOf(
+            module {
+                factory { splashInteractor }
+            }
+        )
+    }
+
+    override fun setup() {
+        super.setup()
+        splashViewModel = SplashViewModel(application)
+        splashViewModel.viewStateLiveData.observeForever(viewStateObserver)
+    }
+
+}
