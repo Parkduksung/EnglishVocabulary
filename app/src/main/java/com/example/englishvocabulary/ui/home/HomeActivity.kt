207500105a2c38b5a53cfd03497c11f34c323857
Parkduksung <duksung1234@naver.com>
2021-07-28 18:02:50 +0900

    [refactor_home] Home 화면 좀더 가독성있게 수정. (viewpager2 로 변경 등)

--- app/src/main/java/com/example/englishvocabulary/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/englishvocabulary/ui/home/HomeActivity.kt
@@ -1,10 +1,12 @@
 package com.example.englishvocabulary.ui.home
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.englishvocabulary.R
 import com.example.englishvocabulary.base.BaseActivity
 import com.example.englishvocabulary.data.model.ExcelData
@@ -14,83 +16,82 @@ import com.example.englishvocabulary.ui.home.bookmark.RenewBookmarkListener
 import com.example.englishvocabulary.ui.home.quiz.QuizFragment
 import com.example.englishvocabulary.ui.home.search.SearchFragment
 import com.example.englishvocabulary.ui.home.study.StudyFragment
-import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home),
     RenewBookmarkListener {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startViewPagerAndTabLayout()
+        initViewPager()
     }
 
 
-    private fun startViewPagerAndTabLayout(){
+    /**
+     * 첫 화면 구성 (ViewPager/TabLayout)
+     */
+    @SuppressLint("WrongConstant")
+    private fun initViewPager() {
 
-        val homeTabList = resources.getStringArray(R.array.home_tab)
 
-        binding.run {
-            homePager.adapter = object : FragmentStatePagerAdapter(
-                supportFragmentManager,
-                BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-            ) {
-                override fun getCount(): Int =
-                    homeTabList.size
+        with(binding) {
 
-                override fun getItem(position: Int): Fragment =
-                    when (position) {
-                        0 -> StudyFragment.newInstance()
-                        1 -> QuizFragment.newInstance()
-                        2 -> BookmarkFragment.newInstance()
-                        3 -> SearchFragment.newInstance()
-                        else -> error("Invalid position")
-                    }
-
-                override fun getPageTitle(position: Int): CharSequence? =
-                    homeTabList[position]
-            }
+            val fragmentList = listOf(
+                StudyFragment.newInstance(), QuizFragment.newInstance(),
+                BookmarkFragment.newInstance(), SearchFragment.newInstance()
+            )
 
             homePager.apply {
-                offscreenPageLimit = 4
-                setSwipePagingEnabled(false)
+                adapter = FragmentPagerAdapter(fragmentList, this@HomeActivity)
+                offscreenPageLimit = LIMIT_COUNT
+                isUserInputEnabled = false
             }
 
+            TabLayoutMediator(bottomTab, homePager, tabConfigurationStrategy).attach()
 
-            bottomTab.run {
-                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-                    override fun onTabSelected(tab: TabLayout.Tab?) {
-                    }
-
-                    override fun onTabUnselected(tab: TabLayout.Tab?) {
-                    }
-
-                    override fun onTabReselected(tab: TabLayout.Tab?) {
-                    }
-                })
-
-                setupWithViewPager(homePager)
-                getTabAt(0)?.setIcon(R.drawable.ic_study)
-                getTabAt(1)?.setIcon(R.drawable.ic_quiz)
-                getTabAt(2)?.setIcon(R.drawable.ic_bookmark)
-                getTabAt(3)?.setIcon(R.drawable.ic_search)
-            }
         }
     }
+
+    private val tabConfigurationStrategy =
+        TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+
+            val nameArray = resources.getStringArray(R.array.array_tab_name).toList()
+            val iconArray = resources.getIntArray(R.array.array_tab_icon).toList()
+
+            val tabInfoArray = (nameArray zip iconArray)
+
+            tab.text = tabInfoArray[position].first
+            tab.setIcon(tabInfoArray[position].second)
+        }
 
 
     override fun renewItem(item: ExcelData) {
         supportFragmentManager.fragments.forEach {
             when (it) {
-                is StudyFragment-> {
+                is StudyFragment -> {
                     it.renewItem(item)
                 }
             }
         }
     }
 
+
     companion object {
+
+        private const val LIMIT_COUNT = 4
+
         fun getIntent(context: Context) =
             Intent(context, HomeActivity::class.java)
     }
+}
+
+class FragmentPagerAdapter(
+    private val fragmentList: List<Fragment>,
+    fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount() = fragmentList.size
+    override fun createFragment(position: Int) = fragmentList[position]
 }
