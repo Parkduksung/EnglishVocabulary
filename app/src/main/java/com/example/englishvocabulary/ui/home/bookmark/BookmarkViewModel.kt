21365246da20ea220d12b7a56a94b79e35abffd1
Parkduksung <duksung1234@naver.com>
2021-08-04 13:40:44 +0900

    [refactor-bookmark] toggleBookmark 수정. (겹침.)

--- app/src/main/java/com/example/englishvocabulary/ui/home/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/example/englishvocabulary/ui/home/bookmark/BookmarkViewModel.kt
@@ -6,6 +6,8 @@ import com.example.englishvocabulary.base.BaseViewModel
 import com.example.englishvocabulary.base.ViewState
 import com.example.englishvocabulary.data.model.ExcelData
 import com.example.englishvocabulary.data.repository.ExcelVocaRepository
+import com.example.englishvocabulary.ui.home.study.StudyInteractor
+import kotlinx.coroutines.launch
 import org.koin.java.KoinJavaComponent.inject
 
 class BookmarkViewModel(
@@ -17,6 +19,7 @@ class BookmarkViewModel(
     private val _bookmarkListLiveData = MutableLiveData<List<ExcelData>>()
     val bookmarkListLiveData: LiveData<List<ExcelData>> = _bookmarkListLiveData
 
+    private val studyInteractor by inject(StudyInteractor::class.java)
 
     // 다른페이지에서 다시 돌아올때 갱신되게끔 하기 위해 생명주기를 onResume 에 불리게끔 설정.
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
@@ -28,8 +31,8 @@ class BookmarkViewModel(
 
     // 즐겨찾기 삭제
     fun deleteBookmark(item: ExcelData) {
-        excelVocaRepository.toggleBookmarkExcelData(false, item) {
-            if (it) {
+        viewModelMainScope.launch {
+            if(studyInteractor.toggleBookmarkExcelData(false, item)){
                 viewStateChanged(BookmarkViewState.RenewBookmarkAdapter)
             }
         }
