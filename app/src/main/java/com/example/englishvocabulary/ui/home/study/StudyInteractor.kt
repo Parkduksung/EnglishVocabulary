ab835e9e78c1f5bdd997895749c1465c34b6f485
Parkduksung <duksung1234@naver.com>
2021-08-04 13:23:50 +0900

    [refactor-Study] Interactor 추가

---
+++ app/src/main/java/com/example/englishvocabulary/ui/home/study/StudyInteractor.kt
@@ -0,0 +1,19 @@
+package com.example.englishvocabulary.ui.home.study
+
+import com.example.englishvocabulary.data.repository.ExcelVocaRepository
+import com.example.englishvocabulary.network.room.entity.ExcelVocaEntity
+import com.example.englishvocabulary.util.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.koin.java.KoinJavaComponent.inject
+
+class StudyInteractor {
+
+    private val excelVocaRepository by inject(ExcelVocaRepository::class.java)
+
+    suspend fun getWantExcelVocaData(wantDay: String): Result<List<ExcelVocaEntity>> =
+        withContext(Dispatchers.IO) {
+            return@withContext excelVocaRepository.getWantDayExcelVocaData(wantDay)
+        }
+
+}
