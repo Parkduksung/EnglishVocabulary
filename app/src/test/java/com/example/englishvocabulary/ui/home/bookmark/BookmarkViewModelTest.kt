910532a13d4e41f883492766d5cdae98b8e05f87
Parkduksung <duksung1234@naver.com>
2021-08-14 22:53:17 +0900

    [refactor-bookmark] BookmarkViewModelTest 생성.

---
+++ app/src/test/java/com/example/englishvocabulary/ui/home/bookmark/BookmarkViewModelTest.kt
@@ -0,0 +1,21 @@
+package com.example.englishvocabulary.ui.home.bookmark
+
+import base.ViewModelBaseTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+import org.koin.core.module.Module
+
+class BookmarkViewModelTest : ViewModelBaseTest() {
+
+
+
+
+    override fun createModules(): List<Module> {
+        TODO("Not yet implemented")
+    }
+
+    @ExperimentalCoroutinesApi
+    override fun setup() {
+        super.setup()
+    }
+}
