7285f6f39affd4e22845712d4c647138cc8a5a1b
Parkduksung <duksung1234@naver.com>
2021-08-14 23:03:36 +0900

    [refactor-bookmark] BookmarkViewModelTest 기초.

--- app/src/test/java/com/example/englishvocabulary/ui/home/bookmark/BookmarkViewModelTest.kt
+++ app/src/test/java/com/example/englishvocabulary/ui/home/bookmark/BookmarkViewModelTest.kt
@@ -1,17 +1,20 @@
 package com.example.englishvocabulary.ui.home.bookmark
 
 import base.ViewModelBaseTest
+import com.example.englishvocabulary.data.repository.ExcelVocaRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.*
 import org.koin.core.module.Module
+import org.koin.dsl.module
+import org.mockito.Mock
 
 class BookmarkViewModelTest : ViewModelBaseTest() {
 
-
-
+    @Mock
+    lateinit var excelVocaRepository: ExcelVocaRepository
 
     override fun createModules(): List<Module> {
-        TODO("Not yet implemented")
+        return listOf(module { single { excelVocaRepository } })
     }
 
     @ExperimentalCoroutinesApi
