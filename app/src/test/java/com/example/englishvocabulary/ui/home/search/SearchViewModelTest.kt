f4a9b38626f456b180ef042df228d83ea7e9b91a
Parkduksung <duksung1234@naver.com>
2021-08-16 19:42:58 +0900

    [refactor-search] SearchViewModelTest TestCode 생성. coverage 100

--- app/src/test/java/com/example/englishvocabulary/ui/home/search/SearchViewModelTest.kt
+++ app/src/test/java/com/example/englishvocabulary/ui/home/search/SearchViewModelTest.kt
@@ -2,10 +2,15 @@ package com.example.englishvocabulary.ui.home.search
 
 import base.ViewModelBaseTest
 import com.example.englishvocabulary.data.repository.SearchRepository
+import com.example.englishvocabulary.network.response.KakaoSearchResponse
+import com.example.englishvocabulary.util.Result
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.mockito.Mock
+import org.mockito.Mockito
 
 class SearchViewModelTest : ViewModelBaseTest() {
 
@@ -29,5 +34,40 @@ class SearchViewModelTest : ViewModelBaseTest() {
         searchViewModel.viewStateLiveData.observeForever(viewStateObserver)
     }
 
+
+    @Test
+    fun checkSearchKakaoWordTest() = runBlocking {
+
+        searchViewModel.searchWordLiveData.postValue("hello")
+
+        Mockito.`when`(searchRepository.searchKakaoWord("hello")).thenReturn(
+            Result.Success(
+                KakaoSearchResponse(listOf(listOf("안녕")))
+            )
+        )
+
+        searchViewModel.searchKakaoWord()
+
+        Mockito.verify(viewStateObserver)
+            .onChanged(SearchViewModel.SearchViewstate.Translate("안녕"))
+
+    }
+
+    @Test
+    fun checkSearchKakaoWordErrorTest() = runBlocking {
+
+        searchViewModel.searchWordLiveData.postValue(null)
+
+        Mockito.`when`(searchRepository.searchKakaoWord(null)).thenReturn(
+            Result.Failure(
+                Throwable("Not Search Word")
+            )
+        )
+
+        searchViewModel.searchKakaoWord()
+
+        Mockito.verify(viewStateObserver)
+            .onChanged(SearchViewModel.SearchViewstate.Error(Throwable("Not Search Word").toString()))
+    }
 
 }
