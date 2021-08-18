4a4e12418f60b311ee53bde37d041b246b993d03
Parkduksung <duksung1234@naver.com>
2021-08-18 22:06:30 +0900

    [refactor-source] ExcelDao 추가.

--- app/src/test/java/com/example/englishvocabulary/data/source/local/excelvoca/ExcelVocaLocalDataSourceImplTest.kt
+++ app/src/test/java/com/example/englishvocabulary/data/source/local/excelvoca/ExcelVocaLocalDataSourceImplTest.kt
@@ -1,16 +1,21 @@
 package com.example.englishvocabulary.data.source.local.excelvoca
 
 import base.BaseTest
+import com.example.englishvocabulary.network.room.dao.ExcelVocaDao
 import com.example.englishvocabulary.network.room.database.ExcelVocaDatabase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.mockito.Mock
+import org.mockito.Mockito
 
 class ExcelVocaLocalDataSourceImplTest : BaseTest() {
 
     @Mock
     lateinit var excelVocaDatabase: ExcelVocaDatabase
+
+    @Mock
+    lateinit var excelVocaDao: ExcelVocaDao
 
     private lateinit var excelVocaLocalDataSourceImpl: ExcelVocaLocalDataSource
 
@@ -26,6 +31,7 @@ class ExcelVocaLocalDataSourceImplTest : BaseTest() {
     override fun setup() {
         super.setup()
         excelVocaLocalDataSourceImpl = ExcelVocaLocalDataSourceImpl()
+        excelVocaDao = Mockito.mock(ExcelVocaDao::class.java)
     }
 
 }
