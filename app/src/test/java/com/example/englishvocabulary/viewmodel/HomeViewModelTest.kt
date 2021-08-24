ae60deedacadc38c63007450c15ff360bbef39ad
Parkduksung <duksung1234@naver.com>
2021-08-24 22:01:39 +0900

    [refactor-home] TestCode 생성.

--- app/src/test/java/com/example/englishvocabulary/viewmodel/HomeViewModelTest.kt
+++ app/src/test/java/com/example/englishvocabulary/viewmodel/HomeViewModelTest.kt
@@ -1,11 +1,16 @@
 package com.example.englishvocabulary.viewmodel
 
 import base.ViewModelBaseTest
+import com.example.englishvocabulary.data.model.ExcelData
 import com.example.englishvocabulary.interactor.BookmarkInteractor
+import com.example.englishvocabulary.util.Result
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.mockito.Mock
+import org.mockito.Mockito
 
 class HomeViewModelTest : ViewModelBaseTest() {
 
@@ -27,5 +32,20 @@ class HomeViewModelTest : ViewModelBaseTest() {
         super.setup()
         homeViewModel = HomeViewModel(application)
         homeViewModel.viewStateLiveData.observeForever(viewStateObserver)
+    }
+
+    @Test
+    fun checkToggleBookmarkExcelDataSuccessTest() = runBlocking {
+        val excelData = ExcelData(day = "Day1", word = "resume", mean = "이력서", like = false)
+
+        val successResult = Result.success(excelData.copy(like = true).toExcelVocaEntity())
+
+        Mockito.`when`(bookmarkInteractor.toggleBookmarkExcelData(excelData))
+            .thenReturn(successResult)
+
+        homeViewModel.toggleBookmark(excelData)
+
+        Mockito.verify(viewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.AddBookmark(excelData))
     }
 }
