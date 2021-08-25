d5800077d7ea3ba3ca7423ac9eea1824917fc8b7
Parkduksung <duksung1234@naver.com>
2021-08-25 13:07:24 +0900

    [refactor-home] TestCode 생성. Coverage 100.

--- app/src/test/java/com/example/englishvocabulary/viewmodel/HomeViewModelTest.kt
+++ app/src/test/java/com/example/englishvocabulary/viewmodel/HomeViewModelTest.kt
@@ -3,7 +3,9 @@ package com.example.englishvocabulary.viewmodel
 import base.ViewModelBaseTest
 import com.example.englishvocabulary.data.model.ExcelData
 import com.example.englishvocabulary.interactor.BookmarkInteractor
+import com.example.englishvocabulary.network.room.entity.ExcelVocaEntity
 import com.example.englishvocabulary.util.Result
+import com.nhaarman.mockitokotlin2.any
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -35,7 +37,7 @@ class HomeViewModelTest : ViewModelBaseTest() {
     }
 
     @Test
-    fun checkToggleBookmarkExcelDataSuccessTest() = runBlocking {
+    fun checkToggleBookmarkAddExcelDataSuccessTest() = runBlocking {
         val excelData = ExcelData(day = "Day1", word = "resume", mean = "이력서", like = false)
 
         val successResult = Result.success(excelData.copy(like = true).toExcelVocaEntity())
@@ -47,5 +49,39 @@ class HomeViewModelTest : ViewModelBaseTest() {
 
         Mockito.verify(viewStateObserver)
             .onChanged(HomeViewModel.HomeViewState.AddBookmark(excelData))
+    }
+
+    @Test
+    fun checkToggleBookmarkDeleteExcelDataSuccessTest() = runBlocking {
+        val excelData = ExcelData(day = "Day1", word = "resume", mean = "이력서", like = true)
+
+        val successResult = Result.success(excelData.copy(like = false).toExcelVocaEntity())
+
+        Mockito.`when`(bookmarkInteractor.toggleBookmarkExcelData(excelData))
+            .thenReturn(successResult)
+
+        homeViewModel.toggleBookmark(excelData)
+
+        Mockito.verify(viewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.DeleteBookmark(excelData))
+    }
+
+    @Test
+    fun checkToggleBookmarkExcelDataFailTest() = runBlocking {
+
+        val excelData = ExcelData(day = "Day1", word = "resume", mean = "이력서", like = false)
+
+        val throwable = Throwable("Error ToggleBookmark")
+
+        val failureResult = Result.failure<ExcelVocaEntity>(throwable)
+
+        Mockito.`when`(bookmarkInteractor.toggleBookmarkExcelData(excelData))
+            .thenReturn(failureResult)
+
+        homeViewModel.toggleBookmark(excelData)
+
+        Mockito.verify(viewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.Error("Bookmark Error"))
+
     }
 }
